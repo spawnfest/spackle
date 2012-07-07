68651d1d2202793366a80ccde7c3e4b9baad7425
Mark Allen <mrallen1@yahoo.com>
2012-07-07 00:22:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Spackle
+-------
+
+This is an Erlang application designed to translate an Erlang data structure into a SQL statement.  Think of it
+as kind of a thin ORM layer.
+
---
+++ src/spackle_app.erl
@@ -0,0 +1,20 @@
+-module(spackle_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    sasl:start(),
+    spackle_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+foo() ->
+    ok.
